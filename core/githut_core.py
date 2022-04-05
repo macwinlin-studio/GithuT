604e04f79c50209bc9ff364d774604f70f3ede09
xinxin2021 <xinxinx2021@outlook.com>
2022-04-05 17:51:54 +0800

    Add two cores

---
+++ core/githut_core.py
@@ -0,0 +1,248 @@
+# coding=utf-8
+import github,sqlite3
+from os.path import realpath
+from os.path import dirname
+from sys import path as sys_path
+from webbrowser import open_new as web_open
+from requests import get as requests_get
+sys_path.append(dirname(realpath(__file__)))
+from language_core import languageC
+# project: GitHub Tools Core
+# file: githut_core.py
+# author: MacWinLin Studio CGK Team
+# email: mwl@macwinlin.ml
+# version: LTS(Long Term Support) 1.0
+# Publish only on GitHub and MacWinLin Studio's GitLab.
+# Copyright 2022 MacWinLin Studio.All rights reserved.
+# This is core,if you try to make a unofficial GUI client,please not view gui/shell's codes.Maybe you will view many bugs.
+# Make unofficial GUI client please contact mwl@macwinlin.ml.
+
+# Code
+
+# Load Basic Information,Autologin
+basic = None
+def loadB():
+    global basic
+    con = sqlite3.connect('.mwl-githut-data.db')
+    cur = con.cursor()
+    cur.execute('select * from data')
+    cache = cur.fetchone()
+    basic = [cache[2],cache[3],cache[4],cache[5]]
+    cur.close()
+    con.close()
+loadB()
+
+# Record Login State
+loginV = 0
+# Record Login Basic Information
+g = None
+user = None
+l = None
+# A Language Class
+language = languageC()
+# WARN!Try To Use Language Function,Please Use Reload Function First!Otherwise,Will Error!
+# And If You Change Language,Please Restart Or Use Reload Function!
+language.reload()
+# Space Data Function
+def par(data2):
+    data = data2 + ' '
+    turn = []
+    cache = ''
+    for i in range(len(data)):
+        if data[i] == ' ':
+            turn.append(cache)
+            cache = ''
+        else:
+            cache += data[i]
+    return turn
+# Get Start To -1's Text
+def get(start,text):
+    return(text[start:-1] + text[-1])
+# About
+def githut(data):
+    if data == '':
+        print(language.githutH)
+    elif data in ['-p','--program']:
+        # About Program
+        print(language.aboutPMT)
+        print(language.aboutPMTgtv)
+        print(language.aboutPMTpgv)
+        print(language.aboutPMTpyv)
+        print(language.aboutPMTgithub)
+    elif data in ['-a','--author']:
+        # About Author
+        print(language.aboutAMT)
+        print(language.aboutAMTauthor)
+        print(language.aboutAMTmainD)
+        print(language.aboutAMTverN)
+        print(language.aboutAMTblog)
+    elif data in ['-po','--program-open']:
+        web_open('https://github.com/macwinlin-studio/GithuT/tree/1.0.0')
+    elif data in ['-ao','--author-open']:
+        web_open('https://blog.macwinlin.ml')
+    elif data in ['-l','--license']:
+        try:
+            requests_get('https://test.xinxin2021.tk/itest')
+        except:
+            print(language.licenseE)
+        else:
+            print(requests_get('https://githut.macwinlin.ml/LICENSE').text)
+    elif data in ['-c','--copyright']:
+        print('Copyright © 2022 MacWinLin Studio.All rights reserved.')
+    else:
+        print(language.cerror)
+# Config Language And Token
+def config(data):
+    if data == '':
+        # Helper
+        print(language.configH)
+    # Config 
+    elif data[0:5] == 'token':
+        cache = get(6,data)
+        cache2 = par(cache)
+        con = sqlite3.connect('.mwl-githut-data.db')
+        cur = con.cursor()
+        cur.execute("select * from data")
+        cache3 = cur.fetchone()
+        if cache2[0] == 'add':
+            if cache3[2] == 0:
+                cur.execute("UPDATE data SET token=? WHERE id=?",(cache2[1],1))
+                cur.execute("UPDATE data SET htoken=1 WHERE id=1")
+                con.commit()
+                basic[0] = 1
+                basic[1] = cache2[1]
+            else:
+                print(language.hToken)
+        elif cache2[0] == 'remove':
+            if cache3[2] == 1:
+                cur.execute("UPDATE data SET htoken=0 WHERE id=1")
+                con.commit()
+                basic[0] = 0
+            else:
+                print(language.nhToken)
+        else:
+            print(language.cerror)
+        cur.close()
+        con.close()
+    # Config Language
+    elif data[0:8] == 'language':
+        cache = get(9,data)
+        con = sqlite3.connect('.mwl-githut-data.db')
+        cur = con.cursor()
+        if cache in language.lList:
+            cur.execute("UPDATE data SET language='" + cache + "' WHERE id=1")
+            con.commit()
+        else:
+            print(language.cerror)
+        language.reload()
+        cur.close()
+        con.close()
+    # Config Autologin
+    elif data[0:9] == 'autologin':
+        cache = get(10,data)
+        con = sqlite3.connect('.mwl-githut-data.db')
+        cur = con.cursor()
+        if cache in ['-y','--yes','-n','--no']:
+            if cache == '-y' or cache == '--yes':
+                if basic[3] == 0:
+                    cur.execute("UPDATE data SET alogin=1 WHERE id=1")
+                    con.commit()
+                    basic[3] = 1
+                    print(language.alY)
+                else:
+                    print(language.NalY)
+            else:
+                if basic[3] == 1:
+                    cur.execute("UPDATE data SET alogin=0 WHERE id=1")
+                    con.commit()
+                    basic[3] = 0
+                    print(language.alN)
+                else:
+                    print(language.NalN)
+        else:
+            print(language.cerror)
+        cur.close()
+        con.close()
+    else:
+        print(language.cerror)
+# Set Account Type
+def account(data):
+    # Helper
+    if data == '':
+        print(language.accountH)
+    elif data in ['-o','--organization','-u','--user']:
+        con = sqlite3.connect('.mwl-githut-data.db')
+        cur = con.cursor()
+        if data in ['-o','--organization']:
+            # Execute The Command
+            if basic[2] == 0:
+                basic[2] = 1
+                cur.execute("UPDATE data SET account=1 WHERE id=1")
+                con.commit()
+            print(language.iso)
+        else:
+            # Execute The Command
+            if basic[2] == 1:
+                basic[2] = 0
+                cur.execute("UPDATE data SET account=0 WHERE id=1")
+                con.commit()
+            print(language.isu)
+    else:
+        print(language.cerror)
+# Login
+def loginF():
+    global g
+    global user
+    global l
+    global login
+    g = github.Github(login_or_token=basic[1])
+    if basic[2] == 1:
+        user = g.get_organization()
+    else:
+        user = g.get_user()
+    if basic[0] == 0:
+        print(language.nhToken)
+    else:
+        try:
+            l = user.login
+        except:
+            print(language.terror)
+        else:
+            cache = language.loginS
+            if basic[2] == 1:
+                cache += language.loginSO
+            else:
+                cache += language.loginSU
+            cache += l
+            print(cache)
+            login = 1
+# Run Command
+def run(data):
+    if data == 'help':
+        print(language.help)
+    elif data[0:6] == 'config':
+        if data == 'config':
+            config('')
+        else:
+            config(get(7,data))
+    elif data[0:6] == 'githut':
+        if data == 'githut':
+            githut('')
+        else:
+            githut(get(7,data))
+    elif data[0:7] == 'account':
+        if data == 'account':
+            account('')
+        else:
+            account(get(8,data))
+    elif data == 'login':
+        loginF()
+    else:
+        print("'{}'".format(data) + language.notc)
+# Rebuild Database
+def redata(data):
+    pass
+
+if basic[3] == 1:
+    print(language.alogin)
+    loginF()
---
+++ core/language_core.py
@@ -0,0 +1,139 @@
+# coding=utf-8
+import sqlite3,pathlib
+from os.path import isfile as os_path_isfile
+from locale import getdefaultlocale as getlanguage
+# project: GitHub Tools Language Core
+# file: language_core.py
+# author: MacWinLin Studio CGK Team
+# email: mwl@macwinlin.ml
+# version: LTS(Long Term Support) 1.0
+# Publish only on GitHub and MacWinLin Studio's GitLab.
+# Copyright 2022 MacWinLin Studio.All rights reserved.
+
+# Code
+
+# Create Database
+if not os_path_isfile('.mwl-githut-data.db'):
+    con = sqlite3.connect('.mwl-githut-data.db')
+    cur = con.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS data(id INTEGER PRIMARY KEY,language TEXT NOT NULL DEFAULT 'en-us',htoken INTEGER DEFAULT 0,token TEXT,account INTEGER NOT NULL DEFAULT 0,alogin INTEGER NOT NULL DEFAULT 0)")
+    cur.execute("INSERT INTO data values (1,'en-us',0,'123456',0,0)")
+    con.commit()
+    # Change Default Language,Only Support Simplified Chinese
+    if 'zh' in getlanguage()[0]:
+        cur.execute("UPDATE data SET language='zh-cn' WHERE id=1")
+        con.commit()
+    cur.close()
+    con.close()
+# Language Function
+class languageC():
+    def __init__(self):
+        # Add Language Please Change This List
+        self.lList = ['zh-cn','en-us']
+        # Language Module Won't Load Language,Please Use Reload Function To Load Language
+    def readLanguage():
+        con = sqlite3.connect('.mwl-githut-data.db')
+        cur = con.cursor()
+        cur.execute('select * from data')
+        cache = cur.fetchone()
+        cur.close()
+        con.close()
+        return cache[1]
+    def reload(self):
+        if languageC.readLanguage() == 'zh-cn':
+            self.aboutPMT = '关于程序'
+            self.aboutPMTgtv = '版本:LTS(长期支持)1.0'
+            self.aboutPMTpgv = 'PyGithub版本:1.55'
+            self.aboutPMTpyv = '上次测试Python版本:3.9.1'
+            self.aboutPMTgithub = '打开GitHub仓库,请使用githut -po'
+            self.aboutAMT = '关于作者'
+            self.aboutAMTauthor = '作者:MacWinLin工作室'
+            self.aboutAMTmainD = '主开发者:Xinxin2021'
+            self.aboutAMTverN = '版本更新时间:2022年3月26日'
+            self.aboutAMTblog = '打开MWL工作室博客,请使用githut -ao'
+            self.githutH = '''您可以使用这些命令：
+-a | --author    关于作者
+-ao | --author-open    打开MacWinLin工作室博客
+-p | --program    关于程序
+-po | --program-open 打开GitHub仓库
+-l | --license    查看开源协议
+-c | --copyright    查看版权信息'''
+            self.cerror = '命令错误。'
+            self.notc = '不是命令。'
+            self.help = '''你可以使用这些命令：
+githut    关于
+config    配置信息
+account   设置账号类型
+login     登录'''
+            self.hToken = '请先删除Token。'
+            self.nhToken = '请先添加Token。'
+            self.configH = '''您可以使用这些命令：
+language <language>               更改GithuT语言
+token <github-token>              更改GitHub Token
+autologin [-y/--yes | -n/--no]    更改自动登录状态'''
+            self.licenseE = '无法加载协议，请确认您连接到网络。'
+            self.accountH = '''您可以使用这些命令：
+-u | --user         设置账号类型为个人
+-o | --organization 设置账号类型为组织'''
+            self.iso = '账号类型已设置为组织。'
+            self.isu = '账号类型已设置为个人。'
+            self.terror = '无法登陆，请确认您的Token正确！'
+            self.loginS = '登录成功！'
+            self.loginSO = '组织名称：'
+            self.loginSU = '用户名：'
+            self.alY = '自动登录已启用！'
+            self.alN = '自动登录已禁用！'
+            self.NalY = '请先禁用自动登录！'
+            self.NalN = '请先启用自动登录！'
+            self.alogin = '开始自动登录！'
+        else:
+            # About Program Part
+            self.aboutPMT = 'About Program'
+            self.aboutPMTgtv = 'Version:LTS(Long Term Support) 1.0'
+            self.aboutPMTpgv = 'PyGithub Version:1.55'
+            self.aboutPMTpyv = 'Last Test Python Version:3.9.1'
+            self.aboutPMTgithub = 'Open GitHub Repository,Please Use githut -po'
+            # About Author Part
+            self.aboutAMT = 'About Author'
+            self.aboutAMTauthor = 'Author:MacWinLin Studio'
+            self.aboutAMTmainD = 'Main Developer:Xinxin2021'
+            self.aboutAMTverN = 'Version Updated Time:March 26, 2022'
+            self.aboutAMTblog = 'Open MWL Studio\'s Blog,Please Use githut -am'
+            # Githut Helper Text
+            self.githutH = '''You can use these command:
+-a | --author          About Author
+-ao | --author-open    Open MacWinLin Studio's Blog
+-p | --program         About Program
+-po | --program-open   Open GitHub Repository
+-l | --license         See This Project's License
+-c | --copyright       See This Project's Copyright'''
+            self.cerror = 'Command error.'
+            self.notc = ' is not a command.'
+            # Global Helper Text
+            self.help = '''You can use these command:
+githut    About
+config    Config Information
+account   Set The Account Type
+login     Login GitHub Account'''
+            # About Token Text
+            self.hToken = 'Please delete the token first.'
+            self.nhToken = 'Please add the token first.'
+            self.configH = '''You can use these command:
+language <language>               Change GithuT Language
+token <github-token>              Change GitHub Token
+autologin [-y/--yes | -n/--no]    Change Autologin State'''
+            self.licenseE = 'Could\'t load license,please confirm you connect the network.'
+            self.accountH = '''You can use these command:
+-u | --user           Set The Account Type To Individual
+-o | --organization   Set The Account Type To Organization'''
+            self.iso = 'The Account Type Is Set To Organization.'
+            self.isu = 'The Account Type Is Set To Individual.'
+            self.terror = 'Could\'t login,please confirm your token is right!'
+            self.loginS = 'Login successful!'
+            self.loginSO = 'Username:'
+            self.loginSU = 'Organization Name:'
+            self.alY = 'Autologin is enable now!'
+            self.alN = 'Autologin is disable now!'
+            self.NalY = 'Please disable autologin first!'
+            self.NalN = 'Please enable autologin first!'
+            self.alogin = 'Autologin started!'
